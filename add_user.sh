8b9e89a10faeee5ae22cb6fe3877a8300949f6d1
juancra264 <juancra264@hotmail.com>
2018-09-14 13:14:47 -0500

    script to add user default password

---
+++ add_user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to add a user to the system
+
+#default password
+password="BT.l4t4m"
+
+if [ $(id -u) -eq 0 ]; then
+    read -p "Username : " username
+    #read -s -p "Enter password : " password
+    egrep "^$username" /etc/passwd >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "$username exists, please check"
+        exit 1
+    else
+        useradd -m -p $password -s /bin/bash  $username
+        # Force the user to change password at the next logon
+        passwd -e $username
+        [ $? -eq 0 ] && echo "User has been added..\n\n\tDefault password: BT.l4t4m\n\nThe user should change it after first logon" || echo "Failed to add a user!"
+    fi
+else
+    echo "Only root or suders may add a user to the system"
+    exit 2
+fi
